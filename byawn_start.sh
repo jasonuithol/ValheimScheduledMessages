6c11088f16a0e0c393c128f238915b1052e0eefb
Jason Uithol <jasonuithol@gmail.com>
2026-03-25 11:12:55 +1000

    fixed server startup script

--- byawn_start.sh
+++ byawn_start.sh
@@ -1,1 +1,1 @@
-./run_bepinex.sh  -name "My server" -port 2456 -world "Dedicated" -password "secret" -crossplay
+./run_bepinex.sh valheim_server.x86_64 -name "My server" -port 2456 -world "Dedicated" -password "secret" -crossplay
